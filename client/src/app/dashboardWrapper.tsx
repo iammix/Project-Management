04767a9bbe6bd8a702b37933db47dfb406719245
iammix <k.mixios@gmail.com>
2024-09-15 22:28:43 +0300

    UPDATE

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,18 +1,42 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect } from 'react'
 import Navbar from "@/app/(components)/Navbar";
+import Sidebar from '@/app/(components)/Sidebar';
+import { Chicle } from 'next/font/google';
+import StoreProvider, { useAppSelector } from './redux';
 
-type Props = {}
 
-const DashboarWrapper = ({ children }:{children:React.ReactNode}) => {
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector((state)=>state.global.isSidebarCollapsed);
+  const isDarkMode = useAppSelector((state)=>state.global.isDarkMode);
+  useEffect(()=> {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  })
+
+
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-        {/*sidebar*/}
-        <main className={'flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64'}>
-            <Navbar/>
-            {children}
-        </main>
+      <Sidebar />
+      <main className={'flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64'}>
+        <Navbar />
+        {children}
+      </main>
     </div>
+  )
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   )
 }
 
-export default DashboarWrapper
+export default DashboardLayout
