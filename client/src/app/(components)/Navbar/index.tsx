5a290c546a9973f74d79f9e6eb212d2b928d81ea
iammix <k.mixios@gmail.com>
2024-09-15 15:28:42 +0300

    UPDATE

---
+++ client/src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Navbar() {
+  return (
+    <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-lack dark:px-4 dark:py-3">Navbar</div>
+  )
+}
+
+export default Navbar
